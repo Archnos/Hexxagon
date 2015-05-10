981940aabe7e5096d34ca798583de9615cc77425
archnos <guillaume.charnay@isty.uvsq.fr>
2015-05-10 17:08:38 +0200

    mon début
    La classe Joueur avec les getter et les setter
    et une classe main vide

---
+++ src/main/java/Joueur.java
@@ -0,0 +1,59 @@
+
+public class Joueur {
+
+	private boolean humain;
+	private String pseudo;
+	public int couleur;
+	public int scoreActuel;
+
+	/**
+	 * @return the humain
+	 */
+	public boolean getHumain() {
+		return humain;
+	}
+
+	/**
+	 * @param humain the humain to set
+	 */
+	public void setHumain(boolean humain) {
+		this.humain = humain;
+	}
+
+	/**
+	 * @return the pseudo
+	 */
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	/**
+	 * @param pseudo the pseudo to set
+	 */
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	
+	/**
+	 * @return the humain
+	 */
+	public int getCouleur() {
+		return couleur;
+	}
+
+	/**
+	 * @param humain the humain to set
+	 */
+	public void setCouleur(int couleur) {
+		this.couleur = couleur;
+	}
+
+	/**
+	 * @return the humain
+	 */
+	public int getScoreActuel() {
+		return scoreActuel;
+	}
+	
+	
+}
